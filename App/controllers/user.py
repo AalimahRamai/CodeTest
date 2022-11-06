c8498d67c53c52dd3e73c2294b40804d810b5bc4
AalimahRamai <60389897+AalimahRamai@users.noreply.github.com>
2022-11-05 21:24:49 -0400

    Update user.py

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,8 +1,8 @@
 from App.models import User
 from App.database import db
 
-def create_user(username, password):
-    newuser = User(username=username, password=password)
+def create_user(username, password, firstName, lastName, jobTitle):
+    newuser = User(username=username, password=password, firstName=firstName, lastName=lastName, jobTitle=jobTitle)
     db.session.add(newuser)
     db.session.commit()
     return newuser
@@ -23,11 +23,43 @@ def get_all_users_json():
     users = [user.toJSON() for user in users]
     return users
 
-def update_user(id, username):
+def update_user(id, username, firstName, lastName, jobTitle):
     user = get_user(id)
     if user:
         user.username = username
+        user.firstName = firstName
+        user.lastName = lastName
+        user.jobTitle = jobTitle
         db.session.add(user)
         return db.session.commit()
     return None
     
+"""
+def create_user(username, password):
+    newuser = User(username=username, password=password)
+    db.session.add(newuser)
+    db.session.commit()
+    return newuser
+def get_user_by_username(username):
+    return User.query.filter_by(username=username).first()
+def get_user(id):
+    return User.query.get(id)
+def get_all_users():
+    return User.query.all()
+def get_all_users_json():
+    users = User.query.all()
+    if not users:
+        return []
+    users = [user.toJSON() for user in users]
+    return users
+def update_user(id, username, firstName, lastName, jobTitle):
+    user = get_user(id)
+    if user:
+        user.username = username
+        user.firstName = firstName
+        user.lastName = lastName
+        user.jobTitle = jobTitle
+        db.session.add(user)
+        return db.session.commit()
+    return None 
+"""
