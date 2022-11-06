4d1e473f2b1c15f63831f27a4c4359221ede5381
AalimahRamai <60389897+AalimahRamai@users.noreply.github.com>
2022-11-05 21:27:38 -0400

    Create review.py

---
+++ App/controllers/review.py
@@ -0,0 +1,55 @@
+from App.models import User, Student, Review
+from App.database import db
+
+def log_review(userId, studentId, comment):
+    student = Student.query.get(studentId=studentId)
+    if student:
+        review = Review(userId=userId, studentId=studentId, comment=comment, like=like)
+        db.session.add(review)
+        db.session.commit()
+        return review
+    return False
+
+def delete_review(reviewId, userId, studentId):
+    review = Review.query.filter(reviewId=reviewId, userId=userId, studentId=student)
+    if review:
+        db.session.delete(review)
+        res = db.session.commit()
+        return res
+    return False
+             
+
+def get_review(id):
+    return Review.query.get(id)
+    
+def view_student_reviews(studentId): #view all the reviews about a particular student
+    reviews = Review.query.filter(studentId=studentId).all()
+    if not reviews:
+        return []
+    reviews = [review.toJSON() for review in reviews]
+    return reviews
+
+def view_user_reviews(userId): #a user can view all the reviews made by themselves
+    reviews = Review.query.filter(userId=userId).all()
+    if not reviews:
+        return []
+    reviews = [review.toJSON() for review in reviews]
+    return reviews
+
+def like_review(reviewId, userId, studentId):
+    review = Review.query.filter(reviewId=reviewId, userId=userId, studentId=studentId)
+    if review:
+        review.like = True
+        db.session.add(review)
+        db.session.commit()
+        return review
+    return False
+
+def dislike_review(reviewId, userId, studentId):
+    review = Review.query.filter(reviewId=reviewId, userId=userId, studentId=studentId)
+    if review:
+        review.dislike = True
+        db.session.add(review)
+        db.session.commit()
+        return review
+    return False  
